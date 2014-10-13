c7ce094ae527aaebf288c2dea04c732d30089426
Ryan Siska <rsiska1@gmail.com>
2014-10-13 14:52:28 -0500

    Wrote the conversion from JSON to useable URL

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -9,15 +9,37 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property NSArray *eventsInfoArray;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=5c141e6f197b202950a3f4d15345f26"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", jsonString); //used to see if correctly retrieving data
+
+        NSError *jsonError = nil;
+
+        if (data)
+        {
+            self.eventsInfoArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+            [self.tableView reloadData];
+        }
+
+        NSLog(@"Connection Error: %@", connectionError);
+        NSLog(@"JSON Error: %@", jsonError);
+    }];
 }
+
 
 
 
